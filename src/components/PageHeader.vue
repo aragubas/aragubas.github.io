18eb7848ae34204c38c44af052345c9042e5ad70
Aragubas <47148297+aragubas@users.noreply.github.com>
2022-07-13 23:11:15 -0300

    Dist

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,7 +4,7 @@
 
 <template>
 <div class="wrapper">
-  <img src="/aragubas.svg" aria-labelledby="header-title" alt="Aragubas Logo" />
+  <img src="/aragubas.svg" id="header-logo" aria-labelledby="header-title" alt="Aragubas Logo" />
   <p id="header-title">Aragubas</p>
 </div>
 </template>
@@ -14,13 +14,21 @@
 {
   padding: .5rem 1rem .5rem 1rem;
   display: flex;
+  gap: .5rem;
+  align-items: center;
   background-color: var(--background-separation);
   height: 2rem;
+}
+
+#header-logo
+{
+  width: 2.5rem;
 }
 
 #header-title
 {
   font-family: "Ubuntu";
+  font-size: 2rem;
   font-weight: bold;
 }
 
