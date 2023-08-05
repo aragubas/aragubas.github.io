633b3eab5421cbbdd2591d2b8703fc06d72ead0a
Aragubas <47148297+aragubas@users.noreply.github.com>
2023-08-04 22:10:34 -0300

    e

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: { outDir: "./docs" },
+  build: { outDir: "./build" },
   base: "./"
 })
