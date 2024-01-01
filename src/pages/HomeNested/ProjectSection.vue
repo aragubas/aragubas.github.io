15a34e848816cc3127cfc3c259cf3f2e2bed02c0
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-01-01 07:48:10 -0300

    Added LiVerse

--- src/pages/HomeNested/ProjectSection.vue
+++ src/pages/HomeNested/ProjectSection.vue
@@ -13,6 +13,15 @@
 
     <article class="project-card">
       <h4 class="title">
+        LiVerse
+        <span class="language-label" language="c#">C#</span>
+      </h4>
+      <p>Cross-Platform Application build on my own GUI Framework for PNGTube Avatars</p>
+      <a href="https://github.com/aragubas/LiVerseSharp" target="_blank" class="button">Github</a>
+    </article>
+
+    <article class="project-card">
+      <h4 class="title">
         mkOsSierraBreeze
         <span class="language-label" language="c++">C++</span>
       </h4>
@@ -20,7 +29,7 @@
       <p>Based on <a href="https://github.com/kupiqu/SierraBreezeEnhanced">Sierra Breeze Enhanced</a></p>
       <a href="https://github.com/aragubas/mkOsSierraBreeze" target="_blank" class="button">Github</a>
     </article>
-    
+
     <article class="project-card">
       <h4 class="title">Video Cruncher <span class="language-label" language="python">Python</span></h4>
       <p>Interactive CLI Tool for recursive video compression</p>
@@ -64,7 +73,8 @@
         ChargerPlugEvent
         <span class="language-label" language="c#">C#</span>
       </h4>
-      <p><i>(Windows Only)</i> Displays a notification and plays a sound when plugging/unplugging laptop charger</p>
+      <p><i>(Windows Only)</i> Displays a notification and plays a sound when plugging/unplugging laptop charger
+      </p>
       <a href="https://github.com/aragubas/ChargerPlugEvent" target="_blank" class="button">Github</a>
     </article>
 
@@ -96,8 +106,10 @@
       <h4 class="title">🗣<span class="language-label" language="css">CSS</span></h4>
       <p>aiajsdkafoefpokwepfdoedpefw</p>
       <div class="flex gap-small-m">
-        <a href="https://github.com/aragubas/aiajsdkafoefpokwepfdoedpefw" target="_blank" class="button">Github</a>
-        <a href="https://aragubas.github.io/aiajsdkafoefpokwepfdoedpefw/" target="_blank" class="button">Website</a>
+        <a href="https://github.com/aragubas/aiajsdkafoefpokwepfdoedpefw" target="_blank"
+           class="button">Github</a>
+        <a href="https://aragubas.github.io/aiajsdkafoefpokwepfdoedpefw/" target="_blank"
+           class="button">Website</a>
       </div>
     </article>
   </section>
