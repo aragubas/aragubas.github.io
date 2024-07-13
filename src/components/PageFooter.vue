356ce756f5de0806b290df2b8fa60fecd7c1f7e2
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-07-12 23:39:13 -0300

    Add cool border to footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -3,81 +3,89 @@ import { ref } from "@vue/reactivity";
 </script>
 
 <template>
-  <footer id="wrapper">
-    <section id="contact-links">
-      <h4 class="title">Contact me</h4>
+  <div class="border">
 
-      <ul class="flex gap-small-m link-list">
-        <li>
-          <font-awesome-icon icon="fa-solid fa-inbox" />
-          <a href="mailto:vaiogames18@gmail.com" target="_blank">Email</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-brands fa-telegram" />
-          <a href="https://t.me/aragubas" target="_blank">Telegram</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-brands fa-linkedin" />
-          <a href="https://www.linkedin.com/in/paulo-otavio-lima-357344264/" target="_blank">LinkedIn</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-brands fa-discord" />
-          <p>@aragubas</p>
-        </li>
-      </ul>
-    </section>
-
-    <section id="social-links">
-      <h4 class="title">Social</h4>
-
-      <ul class="flex gap-small-m link-list">
-        <li>
-          <font-awesome-icon icon="fa-brands fa-youtube" />
-          <a href="https://youtube.com/@aragubas" target="_blank">Youtube</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-solid fa-paw" />
-          <a href="https://www.furaffinity.net/user/aragubas" target="_blank">Furaffinity</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-brands fa-mastodon" />
-          <a href="https://furry.engineer/@aragubas" target="_blank">Mastodon</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-brands fa-twitter" />
-          <a href="https://twitter.com/Aragubas_" target="_blank">Twitter/X</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-brands fa-instagram" />
-          <a href="https://www.instagram.com/aragubas" target="_blank">Instagram</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-brands fa-tiktok" />
-          <a href="https://www.tiktok.com/@aragubas" target="_blank">TikTok</a>
-        </li>
-        <li>
-          <font-awesome-icon icon="fa-solid fa-cloud" />
-          <a href="https://bsky.app/profile/aragubas.bsky.social" target="_blank">Bluesky</a>
-        </li>
-      </ul>
-    </section>
-
-    <section>
-      <p>
-        Made with <a href="https://vuejs.org/" target="_blank">VueJS</a> using the
-        <a href="https://fontawesome.com/" target="_blank">Font Awesome</a> library!
-      </p>
-      <p>
-        Source code available at <a href="https://github.com/aragubas/aragubas.github.io" target="_blank">Github</a>!
-      </p>
-      <p>Bwah!</p>
-    </section>
-
-    <q>The essential is invisible to the eye</q>
-  </footer>
+    <footer id="wrapper">
+      <section id="contact-links">
+        <h4 class="title">Contact me</h4>
+  
+        <ul class="flex gap-small-m link-list">
+          <li>
+            <font-awesome-icon icon="fa-solid fa-inbox" />
+            <a href="mailto:vaiogames18@gmail.com" target="_blank">Email</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-brands fa-telegram" />
+            <a href="https://t.me/aragubas" target="_blank">Telegram</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-brands fa-linkedin" />
+            <a href="https://www.linkedin.com/in/paulo-otavio-lima-357344264/" target="_blank">LinkedIn</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-brands fa-discord" />
+            <p>@aragubas</p>
+          </li>
+        </ul>
+      </section>
+  
+      <section id="social-links">
+        <h4 class="title">Social</h4>
+  
+        <ul class="flex gap-small-m link-list">
+          <li>
+            <font-awesome-icon icon="fa-brands fa-youtube" />
+            <a href="https://youtube.com/@aragubas" target="_blank">Youtube</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-solid fa-paw" />
+            <a href="https://www.furaffinity.net/user/aragubas" target="_blank">Furaffinity</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-brands fa-mastodon" />
+            <a href="https://furry.engineer/@aragubas" target="_blank">Mastodon</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-brands fa-twitter" />
+            <a href="https://twitter.com/Aragubas_" target="_blank">Twitter/X</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-brands fa-instagram" />
+            <a href="https://www.instagram.com/aragubas" target="_blank">Instagram</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-brands fa-tiktok" />
+            <a href="https://www.tiktok.com/@aragubas" target="_blank">TikTok</a>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-solid fa-cloud" />
+            <a href="https://bsky.app/profile/aragubas.bsky.social" target="_blank">Bluesky</a>
+          </li>
+        </ul>
+      </section>
+  
+      <section>
+        <p>
+          Made with <a href="https://vuejs.org/" target="_blank">VueJS</a> using the
+          <a href="https://fontawesome.com/" target="_blank">Font Awesome</a> library!
+        </p>
+        <p>
+          Source code available at <a href="https://github.com/aragubas/aragubas.github.io" target="_blank">Github</a>!
+        </p>
+        <p>Bwah!</p>
+      </section>
+  
+      <q>The essential is invisible to the eye</q>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
+.border {
+  border-radius: var(--border-radius-normal);
+  border: 1px solid var(--color-out-border);
+}
+
 #wrapper {
   padding: 1.2rem;
   display: flex;
@@ -86,8 +94,8 @@ import { ref } from "@vue/reactivity";
   align-items: flex-start;
   background-color: var(--background-separation);
   /* height: 4rem; */
-  border-top-left-radius: 12px;
-  border-top-right-radius: 12px;
+  border-top-left-radius: var(--border-radius-normal);
+  border-top-right-radius: var(--border-radius-normal);
   border-top-width: 1px;
   border-top-style: inset;
   border-top-color: var(--color-boundary-border);
