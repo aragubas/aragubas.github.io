41dff5f42d2a1a8679f4e09f42eda674594323b7
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-07-26 16:08:22 -0300

    Move all CSS to page-main.css

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -13,47 +13,8 @@ import Section from "../components/Section.vue";
       <CertificationsSection />
     </Section>
 
-    <Section :title="'Projects'" :description="'My public open source projects!'">
+    <Section :title="'Projects'" :description="'public open source projects'">
       <ProjectSection />
     </Section>
   </main>
 </template>
-
-<style scoped>
-main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 3rem;
-  margin-top: 1rem;
-  gap: 1rem;
-}
-
-/* For mobile */
-@media screen and (max-width: 640px) {
-  main {
-    align-items: center;
-    padding: 1rem 0.5rem;
-  }
-
-  .main-section {
-    margin: 0;
-  }
-}
-
-/* For Mobile */
-@media screen and (max-width: 580px) {
-  main {
-    margin: 2rem 0.25rem;
-    margin-top: 1rem;
-  }
-}
-
-/* For big screens (tested on 1600x900) */
-@media screen and (min-width: 1280px) {
-  main {
-    margin: 2rem 5rem;
-    margin-top: 1rem;
-  }
-}
-</style>
