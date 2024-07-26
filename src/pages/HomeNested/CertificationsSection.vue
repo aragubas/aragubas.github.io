65a802bb9d3def5d4261a4f57aaac74aceb560c4
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-07-26 12:01:39 -0300

    Move certifications to GridSection

--- src/pages/HomeNested/CertificationsSection.vue
+++ src/pages/HomeNested/CertificationsSection.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import ProjectCard from '../../components/ProjectCard.vue';
-import { LanguageLabel, Project } from '../../models/Project';
-import { TitleLink } from '../../models/TitleLink';
+import GridSection from "../../components/GridSection.vue";
+import { LanguageLabel, Project } from "../../models/Project";
+import { TitleLink } from "../../models/TitleLink";
 
 const certifications = [
-  new Project("Foundational C# with Microsoft",
+  new Project(
+    "Foundational C# with Microsoft",
     "At March 13, 2024. I completed freeCodeCamp's Foundational C# with Microsoft course!",
-    new Array<LanguageLabel>(
-      LanguageLabel.CSharp
-    ),
+    new Array<LanguageLabel>(LanguageLabel.CSharp),
     new Array<TitleLink>(
-      new TitleLink("View Certificate", "https://www.freecodecamp.org/certification/aragubas/foundational-c-sharp-with-microsoft")
+      new TitleLink(
+        "View Certificate",
+        "https://www.freecodecamp.org/certification/aragubas/foundational-c-sharp-with-microsoft"
+      )
     )
-  )
-]
-
+  ),
+];
 </script>
 
 <template>
-  <ProjectCard v-for="project in certifications" :project="project"/>
+  <GridSection :items="certifications" />
 </template>
