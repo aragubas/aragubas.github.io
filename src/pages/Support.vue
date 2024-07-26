ed3f99bcfe48520db010c83b2f0d62d4299de435
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-07-26 18:30:46 -0300

    Update Support.vue

--- src/pages/Support.vue
+++ src/pages/Support.vue
@@ -1,85 +1,110 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+class DonationService {
+  service: string;
+  address: string;
+  isLink: boolean;
+  linkText: string;
+
+  constructor(service: string, address: string, isLink = false, linkText = "") {
+    this.service = service;
+    this.address = address;
+    this.isLink = isLink;
+    this.linkText = linkText;
+  }
+}
+
+const coins: DonationService[] = [
+  new DonationService("Bitcoin", "1GTDfR9vWiWY29dRXMSQFQ5ss8rCCdA6fR"),
+  new DonationService("Litecoin", "LLJqsw1g26cTXwJQwzGisxui3VjGDUd8mQ"),
+  new DonationService("Doge", "DE9FfvPyL3DxS9y7cUTTGfy1orHD8TqVB1"),
+];
+
+const dnservices: DonationService[] = [
+  new DonationService("PayPal", "dpaulootavio5@outlook.com"),
+  new DonationService("Buy Me a Coffee", "https://www.buymeacoffee.com/aragubas", true, "@aragubas"),
+];
+</script>
 
 <template>
-  <div id="wrapper">
-    <section>
+  <main>
+    <header>
       <div class="clip-box">
-        <h1 class="title">Support!</h1>
+        <h1 class="title">Support</h1>
       </div>
-      <p>
-        I accept donations internationally through
-        <a href="https://www.buymeacoffee.com/aragubas" target="_black">Buy me a coffee</a>
-      </p>
-      <p>and also Crypto:</p>
+    </header>
+    <article>
+      <section>
+        <header>
+          <h1>Regular donation services</h1>
+        </header>
 
-      <ul class="cripto-providers">
-        <li>
-          <p>Bitcoin</p>
-          <code>1GTDfR9vWiWY29dRXMSQFQ5ss8rCCdA6fR</code>
-        </li>
+        <table>
+          <tr>
+            <th>Service</th>
+            <th>Address</th>
+          </tr>
 
-        <li>
-          <p>Litecoin</p>
-          <code>LLJqsw1g26cTXwJQwzGisxui3VjGDUd8mQ</code>
-        </li>
+          <tr v-for="service in dnservices">
+            <td>{{ service.service }}</td>
+            <td v-if="!service.isLink">{{ service.address }}</td>
+            <td v-else>
+              <a target="_blank" :href="service.address">{{ service.linkText }}</a>
+            </td>
+          </tr>
+        </table>
+      </section>
 
-        <li>
-          <p>Doge</p>
-          <code>DE9FfvPyL3DxS9y7cUTTGfy1orHD8TqVB1</code>
-        </li>
-      </ul>
+      <section>
+        <header>
+          <h1>Crypto Currency</h1>
+          <p>I also accept donations through crypto currency</p>
+        </header>
 
-      <br />
-      <p>If you live in Brazil you can also send me a pix by scanning the image below</p>
+        <table>
+          <tr>
+            <th>Service</th>
+            <th>Address</th>
+          </tr>
 
-      <img src="/pix_donation.png" alt="Chave pix de Paulo Otávio de Lima (eu, aragubas)" />
-    </section>
-  </div>
+          <tr v-for="coin in coins">
+            <td>{{ coin.service }}</td>
+            <td>{{ coin.address }}</td>
+          </tr>
+        </table>
+      </section>
+
+      <section class="pix-section">
+        <header>
+          <h1>Pix</h1>
+          <p>
+            If you live in Brazil you can also send me a pix by<br />
+            scanning the image below, or by using the pix key below
+          </p>
+        </header>
+
+        <img src="/pix_donation.png" alt="Chave pix de Paulo Otávio de Lima (eu, aragubas)" />
+      </section>
+    </article>
+  </main>
 </template>
 
 <style scoped>
-#wrapper {
-  margin: 1rem;
-  min-height: 30rem;
-}
-
-.cripto-providers {
-  display: grid;
-  grid-template-columns: auto auto;
-  gap: 0.5rem 1rem;
-  margin-top: 0.5rem;
-}
-
-.cripto-providers > li {
-  display: contents;
-  position: relative;
-}
-
-/* .cripto-providers > li > p
-{
-  grid-column: 1;
-  
-} */
-
-.cripto-providers > li > code {
+article {
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  max-width: 700px;
 }
 
 section {
   display: flex;
   flex-direction: column;
-  gap: 0.2rem;
-  align-items: center;
-  margin: 0 auto;
-  max-width: 500px;
+  gap: 0.5rem;
 }
 
-section .title {
-  font-size: 2rem;
+section.pix-section {
+  align-items: center;
   text-align: center;
-  font-weight: normal;
-  font-family: "Ubuntu";
-  padding: 0.5rem 0;
 }
 </style>
