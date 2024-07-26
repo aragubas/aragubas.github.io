5589bbab334e8680ae2afbcc8af0205f9d03b098
Aragubas <47148297+aragubas@users.noreply.github.com>
2024-07-26 12:01:21 -0300

    Create GridSection.vue

---
+++ src/components/GridSection.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import ProjectCard from "./ProjectCard.vue";
+import { Project } from "../models/Project";
+
+const props = defineProps({ items: Array<Project> });
+</script>
+
+<template>
+  <section>
+    <ProjectCard v-for="project in items" :project="project"> </ProjectCard>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: min-content;
+  gap: 1rem;
+  text-align: start;
+}
+
+/* For mobile */
+@media screen and (max-width: 640px) {
+  section {
+    grid-template-columns: 1fr;
+    gap: 1rem;
+    box-sizing: border-box;
+    justify-items: stretch;
+    margin: 0 auto;
+  }
+}
+
+/* For big screens (tested on 1600x900) */
+@media screen and (min-width: 1280px) {
+  section {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+</style>
