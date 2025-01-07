78601059e6905e8f0d15bbb3ed5b1a92eb7cd987
Aragubas <47148297+aragubas@users.noreply.github.com>
2025-01-07 16:39:28 -0300

    New Website!

--- src/models/TitleLink.ts
+++
@@ -1,9 +0,0 @@
-export class TitleLink {
-    title: string;
-    link: string;
-
-    constructor(title: string, link: string) {
-        this.title = title;
-        this.link = link;
-    }
-}
--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Random number in specified range
- * @returns random <b>float</b> in specified range
- */
-export function randomRange(min: number, max: number): number {
-  return (Math.random() * (max - min) + min);
-}
